0083e612e2b314be11786c0ad3d89dfac3565e27
Brendan Sechter <sgeos@hotmail.com>
2024-03-26 02:25:08 +0900

    Blink logic only updated if user button is depressed.

--- src/main.rs
+++ src/main.rs
@@ -15,35 +15,42 @@ const DELAY: usize = 100_000;
 
 #[entry]
 fn main() -> ! {
-  // Get access to the device specific peripherals, and acquire GPIOA.
+  // Get access to the device specific peripherals, and acquire GPIOA and GPIOC.
   let dp = pac::Peripherals::take().unwrap();
   let mut gpioa = dp.GPIOA.split();
+  let mut gpioc = dp.GPIOC.split();
 
   // Configure GPIO A pin 5 as a push-pull output with the `crl` register.
   // For pins 8-15, use `crh` instead.
   let mut led = gpioa.pa5.into_push_pull_output(&mut gpioa.crl);
+
+  // Button is not mutable because it is read only.
+  let button = gpioc.pc13;
 
   rtt_init_print!();
   rprintln!("Hello, {}!", BOARD);
 
   let mut x: usize = 0;
   loop {
-    // Pre-increment to avoid blinking on first tick.
-    x += 1;
-    if 2 * DELAY < x {
-      x = 0;
-    }
-    match x {
-      0 => {
-        rprintln!("Blink off...");
-        led.set_low();
-      },
-      DELAY => {
-        rprintln!("Blink on...");
-        led.set_high();
-      },
-      // Use ASM nop to slow the cycle down.
-      _ => nop(),
+    // Only update blink logic if button is depressed.
+    if button.is_low() {
+      // Pre-increment to avoid blinking on first tick.
+      x += 1;
+      if 2 * DELAY < x {
+        x = 0;
+      }
+      match x {
+        0 => {
+          rprintln!("Blink off...");
+          led.set_low();
+        },
+        DELAY => {
+          rprintln!("Blink on...");
+          led.set_high();
+        },
+        // Use ASM nop to slow the cycle down.
+        _ => nop(),
+      }
     }
   }
 }
